70890a4537b698e47661d5596d10fbff07de21df
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2023-08-30 19:37:42 +0100

    Day 05 is done

--- 30-Day-of-JavaScript/day_05/script/main.js
+++ 30-Day-of-JavaScript/day_05/script/main.js
@@ -184,5 +184,59 @@ const backEnd = ['Node','Express', 'MongoDB']
 const fullStack = frontEnd.concat(backEnd);
 console.log(fullStack)
 
+/* 
+    Exercise: Level 3
+
+
+    The following is an array of 10 students ages:
+
+    const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+    Sort the array and find the min and max age
+    Find the median age(one middle item or two middle items divided by two)
+    Find the average age(all items divided by number of items)
+    Find the range of the ages(max minus min)
+    Compare the value of (min - average) and (max - average), use abs() method 1.Slice the first ten countries from the countries array
+    Find the middle country(ies) in the countries array
+
+    Divide the countries array into two equal arrays if it is even. If countries array is not even , one more country for the first half.
+*/
+
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
+
+// sort array 
+ages.sort();
+console.log(ages);
+
+// min and max age 
+    // min  
+console.log(ages[0])
+    // max  
+console.log(ages[ages.length - 1]);
+
+// median age 
+console.log(ages[ages.length / 2 - 1] / 2);
+
+// Find the average age(all items divided by number of items)
+let allAges = 0
+for (let i = 0; i < ages.length; i++) {
+    allAges += ages[i];
+}
+
+console.log(`${allAges / ages.length}`);
+
+// Find the range of the ages(max minus min)
+console.log(`range of the age is ${ages[length - 1] - ages[0]}`);
+
+console.log(countries.slice(10));
+
+// Find the middle country(ies) in the countries array did in a lot of times
+console.log(countries.splice(Math.ceil(countries.length / 2) - 1))
+
+
+console.log(countries.splice(0, Math.ceil(countries.length / 2) - 1))
+
+
+
+
 
 
