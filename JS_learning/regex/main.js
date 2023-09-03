a51879e9e6b1ffa856d487e710cfef045093862e
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2023-09-04 00:33:48 +0100

    Form validation using ReGex

--- JS_learning/regex/main.js
+++ JS_learning/regex/main.js
@@ -1,44 +1,30 @@
-/*
-  Regular Expression
+/* 
+    form validations
 
-    Quantifiers
-    n+    => One Or More
-    n*    => zero or more
-    n?    => zero or one
-    n{x}   => Number of
-    n{x,y} => Range
-    n{x,}  => At Least x
-    $  => End With Something
-    ^  => Start With Something
-    ?= => Followed By Something
-    ?! => Not Followed By Something
+    validate input form from user
 */
 
-let urls = 'https://google.com http://www.website.net web.com zakaria.com';
+document.addEventListener('DOMContentLoaded', function() {
 
-// regex pattern for a simple web site
-let urlRe = /(https?:\/\/)?(www.)?\w+.(com|net)/ig ;
+    document.getElementById('register').addEventListener('submit', function(e) {
 
-console.log(urls.match(urlRe));     // output [ 'https://google.com', 'http://www.website.net', 'web.com', 'zakaria.com' ]
+        // phone number
+        let phoneNumber = document.getElementById('phone').value;
 
-// part 2
-let serials = "S100S S3000S S50000S S950000S";
+        // validation pattern  // phone should be like this patter (1234) 123-4567
+        let phoneRe = /\(\d{4}\)\s\d{3}-\d{4}/;
 
-let serialReg = /s\d{3}s/ig;
+        let validationResult = phoneRe.test(phoneNumber);
 
-console.log(serials.match(serialReg)); // Three numbers     output [ 'S100S' ]
-console.log(serials.match(/s\d{4,5}s/ig)); // S [Four or Five numbers]      output  [ 'S3000S', 'S50000S' ]
-console.log(serials.match(/s\d{4,}s/ig)); // S Starting form four numbers       output  [ 'S3000S', 'S50000S', 'S950000S' ]
+        if (validationResult !== true) {
+            // dont send data
+            e.preventDefault();
+            return false;
+        }
 
-// part 3
+        // return true fro submition
+        return true
 
-let something = 'something';
+    })
 
-// start with something wich is 'some'
-console.log(/^some/ig.test(something)); // true
-
-// ends with thing
-console.log(/thing$/ig.test(something)); //true
-
-// followed by something
-console.log(/some(?=thing)/ig.test(something)); // true
+});
