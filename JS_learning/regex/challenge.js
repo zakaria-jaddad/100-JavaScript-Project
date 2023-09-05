a95df2577c77b5ef65043ee72d3fca490f828ab0
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2023-09-05 19:40:05 +0100

    End ReGex assignement

--- JS_learning/regex/challenge.js
+++ JS_learning/regex/challenge.js
@@ -5,24 +5,24 @@ let url4 = 'https://www.elzero.org';
 let url5 = 'https://www.elzero.org:8080/articles.php?id=100&cat=topics';
 let url6 = 'https://elzero.org/javascript-bootcamp-assignments-lesson-from-134-to-146/'
 let url7 = 'https://poe.com/chat/2jpzyznfasl7aylf77b'
-let url8 = 'https://github.com/zakaria-jaddad/100-JavaScript-Project/blob/main/JS_learning/regex/char_classes.js'
+let url8 = 'https://github.com/zakaria-jaddad/100-JavaScript-Project/blob/main/JS_learning/re1gex/char_classes.js'
 let url9 = 'https://www.example.com:8080/path/to/page?param1=value1&param2=value2#fragment';
 let url10 = 'https://subdomain.example.co.uk:8080/path/to/page?param1=value1&param2=value2#fragment/subpath?query=value'
 
 
-let re=/(https?:\/\/)?(www\.)?\w{1,}\.[a-z]{1,}:?(.{1,})?/ig;
+let re1=/(https?:\/\/)?(www\.)?\w{1,}\.[a-z]{1,}:?(.{1,})?/ig;
 
 
-console.log(url1.match(re));
-console.log(url2.match(re));
-console.log(url3.match(re));
-console.log(url4.match(re));
-console.log(url5.match(re));
-console.log(url6.match(re));
-console.log(url7.match(re));
-console.log(url8.match(re));
-console.log(url9.match(re));
-console.log(url10.match(re));
+console.log(url1.match(re1));
+console.log(url2.match(re1));
+console.log(url3.match(re1));
+console.log(url4.match(re1));
+console.log(url5.match(re1));
+console.log(url6.match(re1));
+console.log(url7.match(re1));
+console.log(url8.match(re1));
+console.log(url9.match(re1));
+console.log(url10.match(re1));
 
 /* 
     Output
