efbcb80a2756c230f3ba249039f5847db017de46
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2023-09-12 16:35:16 +0100

    Used then catch finnaly to handel a response

--- JS_learning/promises_and_fetch/main.js
+++ JS_learning/promises_and_fetch/main.js
@@ -1,14 +1,23 @@
-const myPromise = new Promise((rsolveFunction, rejectFunction) => {
-    let connection = false;
+const myPromise = new Promise((resolveFunction, rejectFunction) => {
+    let employees = ["Minnie McGee", "Mamie Wilkins", "Eula Rogers", "Sallie Gordon"];
 
-    // 
-    if (connection) {
-        rsolveFunction("Connetcion Established");
+    // check if ther is 4 employess
+    if (employees.length === 4) {
+        resolveFunction(employees);
     }
-    else {
-        rejectFunction(Error("Connection Field"));
+    else {  
+        rejectFunction(Error("Number of Employees Is Not 4"));
     }
-}).then(
-    (resolveValue => console.log(`Good ${resolveValue}`)), 
-    (rejectedValue) => console.log(`Badd ${rejectedValue}`)
-);
+
+}).then((employees) => {
+
+    // remove two employees 
+    employees.length = 2;
+    return employees;
+})
+.then((employees) => {
+    employees.length = 1;
+    return employees;
+})
+.catch((error) => console.error(error))
+.finally((employees) => console.log("Finally block is executed"));
