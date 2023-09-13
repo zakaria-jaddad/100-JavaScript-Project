4724e1aa484f13f7a91ee45b98451a800a2e981a
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2023-09-13 01:14:49 +0100

    making the same request using fetch and the course is done

--- JS_learning/promises_and_fetch/assignment.js
+++ JS_learning/promises_and_fetch/assignment.js
@@ -7,46 +7,82 @@
 [
 */
 
-const myAssignmentPromise = new Promise((resolve, reject) => {
+// const myAssignmentPromise = new Promise((resolve, reject) => {
 
-    // Request
-    let myAssignmentRequest = new XMLHttpRequest();
+//     // Request
+//     let myAssignmentRequest = new XMLHttpRequest();
 
-    myAssignmentRequest.onload = function () {
+//     myAssignmentRequest.onload = function () {
 
-        // checking for the Response 
-        if (this.readyState === 4 && this.status === 200) {
-            resolve(JSON.parse(this.responseText));
-        }
-        else {
-            reject(Error("Bad Response"));
-        }
-    }
+//         // checking for the Response 
+//         if (this.readyState === 4 && this.status === 200) {
+//             resolve(JSON.parse(this.responseText));
+//         }
+//         else {
+//             reject(Error("Bad Response"));
+//         }
+//     }
 
 
-    myAssignmentRequest.open("GET", "promises_and_fetch/assignment.json")
-    myAssignmentRequest.send();
+//     myAssignmentRequest.open("GET", "promises_and_fetch/assignment.json")
+//     myAssignmentRequest.send();
 
-})
-.then((responseData) => {
+// })
+// .then((responseData) => {
 
-    // itterating over book items from the json file 
-    for (let i = 0; i < 5; i++) {
+//     // itterating over book items from the json file 
+//     for (let i = 0; i < 5; i++) {
 
+        // // heading 3 
+        // const heading = document.createElement("h3");
+        // heading.textContent = responseData[i].title;
+
+        // // p
+        // const paragraph = document.createElement("p");
+        // paragraph.textContent = responseData[i].description;
+
+        // // div container
+        // const conatiner = document.createElement("div");
+        // conatiner.appendChild(heading, paragraph);
+        // conatiner.appendChild(paragraph);
+        // document.body.appendChild(conatiner)
+//     }
+// });
+
+/* 
+    Assignment 02
+    Recreate what was done using fetch function
+*/
+
+fetch("promises_and_fetch/assignment.json")
+    .then(async (response) => {
+        return await response.json()
+    })
+    .then((responseBooks) => {
+        // looping 
+        for (let i = 0; i < 5; i++) {
         // heading 3 
         const heading = document.createElement("h3");
-        heading.textContent = responseData[i].title;
+        heading.textContent = responseBooks[i].title;
 
         // p
         const paragraph = document.createElement("p");
-        paragraph.textContent = responseData[i].description;
+        paragraph.textContent = responseBooks[i].description;
 
         // div container
         const conatiner = document.createElement("div");
         conatiner.appendChild(heading, paragraph);
         conatiner.appendChild(paragraph);
         document.body.appendChild(conatiner)
-    }
-})
+        }
+    })
+    .catch(err => console.error(err))
+    .finally(() => {
+        const div = document.createElement("div")
+        div.textContent = "Operation Of Getting Books Is Done";
+        div.style.color = "red";
+        div.style.fontFamily = "Arial"
 
-
+        // appending last div to the body node
+        document.body.appendChild(div);
+    })
