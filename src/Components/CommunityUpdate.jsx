04c833ac0467416eee7f3bc440e87ee9e4916bc4
fiel777 <jeremiahmonfiel04@gmail.com>
2023-10-08 21:51:03 +0800

    second commit

--- src/Components/CommunityUpdate.jsx
+++ src/Components/CommunityUpdate.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { MdArrowRightAlt } from "react-icons/md";
 function CommunityUpdate() {
   return (
-    <section className="py-20">
+    <section className="py-10 lg:py-20">
       <div className="md:max-w-2xl lg:max-w-4xl xl:max-w-6xl 2xl:max-w-[1440px] m-auto px-4 lg:px-0 flex lg:gap-8 flex-col justify-between gap-4   ">
         <div className="flex flex-col gap-4">
           <h1 className="md:text-3xl text-neutralGray font-bold text-center">
@@ -35,14 +35,15 @@ function CommunityUpdate() {
           </div>
           <div className="relative mb-24 min-[425px]:w-[400px]  md:w-[310px] lg:w-[288px] xl:w-[374px] 2xl:w-[470px]  ">
             <img
-              src="marketingpic1.webp"
+              src="marketingpic2.webp"
               alt="image"
               className="max-w-full max-h-full w-[288px] min-[425px]:w-[450px] 2xl:w-[470px] "
               loading="lazy"
             ></img>
             <div className="max-w-full max-h-full w-[250px] h-[150px] min-[425px]:w-[320px] min-[425px]:h-[175]  md:w-[260px] md:top-[10.5rem] md:left-6 lg:w-[250px] lg:top-[9rem] lg:left-[18.5px] xl:w-[335px] xl:top-[13rem] 2xl:w-[430px] 2xl:top-[17.5rem] 2xl:left-5  bg-naturalSilver absolute top-[10rem] left-5 min-[425px]:top-[15rem] min-[425px]:left-10   shadow-lg rounded-md text-center flex flex-col gap-4 py-8 px-4 ">
               <p className="text-neutralGray text-sm font-medium xl:w-4/5  m-auto">
-                Creating Streamlined Safeguarding Processes with OneRen
+                What are your safeguarding responsibilities and how can you
+                manage them?
               </p>
               <div className="flex items-center justify-center gap-2 cursor-pointer">
                 <span className="text-primary font-semibold ">Read More</span>
@@ -52,14 +53,14 @@ function CommunityUpdate() {
           </div>
           <div className="relative mb-24 min-[425px]:w-[400px]  md:w-[310px] lg:w-[288px] xl:w-[374px] 2xl:w-[470px]  ">
             <img
-              src="marketingpic1.webp"
+              src="marketingpic3.webp"
               alt="image"
               className="max-w-full max-h-full w-[288px] min-[425px]:w-[450px] 2xl:w-[470px] "
               loading="lazy"
             ></img>
             <div className="max-w-full max-h-full w-[250px] h-[150px] min-[425px]:w-[320px] min-[425px]:h-[175]  md:w-[260px] md:top-[10.5rem] md:left-6 lg:w-[250px] lg:top-[9rem] lg:left-[18.5px] xl:w-[335px] xl:top-[13rem] 2xl:w-[430px] 2xl:top-[17.5rem] 2xl:left-5  bg-naturalSilver absolute top-[10rem] left-5 min-[425px]:top-[15rem] min-[425px]:left-10   shadow-lg rounded-md text-center flex flex-col gap-4 py-8 px-4 ">
               <p className="text-neutralGray text-sm font-medium xl:w-4/5  m-auto">
-                Creating Streamlined Safeguarding Processes with OneRen
+                Revamping the Membership Model with Triathlon Australia
               </p>
               <div className="flex items-center justify-center gap-2 cursor-pointer">
                 <span className="text-primary font-semibold ">Read More</span>
@@ -67,7 +68,6 @@ function CommunityUpdate() {
               </div>
             </div>
           </div>
-         
         </div>
       </div>
     </section>
