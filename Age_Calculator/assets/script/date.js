05a3b9d3084da271faa4fd603e2d2c32537d5c36
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2023-10-13 17:37:35 +0100

    Add Time Counter

---
+++ Age_Calculator/assets/script/date.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    // get Date Container 
+    const DateContainer = document.querySelector('#date');
+
+    
+    setInterval(() =>  {
+        const now = new Date() ;
+
+        const dayNumber = now.getDate();
+
+        const dayName = Days[now.getDay()];
+
+        const monthName = Months[now.getMonth()];
+
+        const hour = fixNumber(now.getHours());
+
+        const minuts = fixNumber(now.getMinutes());
+
+        const seconds = fixNumber(now.getSeconds());
+
+        DateContainer.textContent = `${dayName} ${dayNumber} ${monthName} ${hour}:${minuts}:${seconds}`;
+        console.log('hello');
+    }, 1000)
+}) 
+
+function fixNumber(number) {
+    const newNumber = parseInt(number) >= 0 && parseInt(number) < 10 ? '0' + String(number) : number;
+    return newNumber;
+}
+
+const Days = {
+    0 : "Sun", 
+    1 : "Mon", 
+    2 : "Tues", 
+    3 : "Wed", 
+    4 : "Thurs", 
+    5 : "Fri", 
+    6 : "Sat", 
+}
+const Months = {
+    0: "Jan",
+    1: "Feb",
+    2: "Mar",
+    3: "Apr",
+    4: "May",
+    5: "Jun",
+    6: "Jul",
+    7: "Aug",
+    8: "Sep",
+    9: "Oct",
+    10: "Nov",
+    11: "Dec"
+}
+
+/* 
+    Date Formula
+    DD Name DD Number Hours:Minuts
+*/
