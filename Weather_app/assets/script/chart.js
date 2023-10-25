a3774aea70510cfe18646ce16fc4bc2b68041a24
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2023-10-25 17:05:49 +0100

    Full static is done added graph

---
+++ Weather_app/assets/script/chart.js
@@ -0,0 +1,44 @@
+
+setGraph(['e'], [15]);
+
+/* 
+    labels and weatherDate both are Arrays 
+*/
+function setGraph(labels, weatherData) {
+    
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: 'Degree In Celsius',
+            data: weatherData,
+            backgroundColor: [
+            'rgba(256, 256, 256, 0.7)',
+        ],    
+        borderColor: [
+            getComputedStyle(document.documentElement).getPropertyValue('--third-background-color')
+            ],
+        borderWidth: 1
+        }]
+    };
+    
+    const myChart = document.querySelector('[data-today-weather-graph]');
+    
+    console.log(myChart);
+    
+    new Chart(myChart, {
+    
+        type :'bar' , 
+        data : data, 
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    })
+
+}
+
+export {setGraph};
+
