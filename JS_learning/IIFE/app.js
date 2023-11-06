51a8f2b43282e1a96b559cee2c8cdb5b5aee0124
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2023-11-06 15:57:08 +0100

    made todosApp using object method

--- JS_learning/IIFE/app.js
+++ JS_learning/IIFE/app.js
@@ -1,33 +1,62 @@
+// todo app using object 
+const todosApp = {
+
+    todos : [], 
+
+    addTodo : function(todo) {
+        this.todos.push(todo);
+        this.makeTodosLowerCase()
+        this.showTodos()
+        console.log(`You have ${this.todosCounter()} Todos`)
+    }, 
+    deleteTodo : function(todo) {
+        this.todos = this.todos.filter(el => el != todo.toLowerCase());
+    }, 
+    showTodos : function() {
+        console.log(this.todos.join(' | ')); 
+    }, 
+    todosCounter : function() {
+        return this.todos.length;
+    }, 
+    makeTodosLowerCase : function() {
+        this.todos = this.todos.map(todo => todo.toLowerCase());
+    }
+
+
+
+}
+
+
 // spaghity code => making todo app
 
-let todos = []
+// let todos = []
 
-function addTodo(todo) {
+// function addTodo(todo) {
 
-    todos.push(todo)
+//     todos.push(todo)
 
-}
+// }
 
-function deleteTodo(todo) {
+// function deleteTodo(todo) {
 
-    todos = todos.filter(el => el != todo);
+//     todos = todos.filter(el => el != todo);
 
-}
+// }
 
-function makeTodosLowerCase() {
+// function makeTodosLowerCase() {
 
-    todos = todos.map(todo => todo.toLowerCase())
+//     todos = todos.map(todo => todo.toLowerCase())
 
-}
+// }
 
-function showTodos() {
+// function showTodos() {
 
-    console.log(todos.join(' | '));
+//     console.log(todos.join(' | '));
 
-}
+// }
 
-function todosCounter() {
+// function todosCounter() {
 
-    console.log(todos.length);
+//     console.log(todos.length);
 
-}
+// }
