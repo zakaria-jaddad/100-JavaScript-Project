c22b06923e7e835dafa5f2091297d0ecc33eb721
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2023-11-28 14:12:22 +0100

    playing around

--- JS_learning/React/hello-world/src/App.jsx
+++ JS_learning/React/hello-world/src/App.jsx
@@ -1,49 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react"
 
-function App() {
-const [count, setCount] = useState(0)
+export default function App() {
 
-return (
-    <>
-    <h1 className="
-        // bg-gradient-to-bl from-indigo-900 via-indigo-400 to-indigo-900
-        font-bold
-        px-3`
-        py-4
-        rounded-md
-    ">Hello Zakaria From React</h1>
-    <div className="
-        my-4
-        flex
-        justify-center
-    ">
-        <a href="https://vitejs.dev" target="_blank">
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-    </div>
-    <h1>Vite + React</h1>
-    <div className="card">
-        <button className='
-            mb-4
-        ' 
-        onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-        </button>
-        <p>
-        Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-    </div>
-    <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-    </p>
-    </>
-)
+    const aboutMe = {
+        firstName: 'Zakaria', 
+        lastName: 'Jaddad', 
+        age: 20
+    }
+
+    function greeting({ firstName, lastName }) {
+        /* 
+        returns gretting to user by getting
+            - firstName
+            - lastName
+            */
+
+        return (
+            <div>
+                <ul className="
+                    px-4 
+                    list-none
+                ">
+                    <li> { firstName } </li>
+                    <li> { lastName } </li>
+                </ul>
+            </div>
+        )
+
+    }
+    
+    return (
+        <div>
+            <h1>Hello to you, My Name is : </h1>
+            { greeting(aboutMe) }
+        </div>
+    )
 }
 
-export default App
+
