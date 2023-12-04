c4f70313c15b3e9f72e1c6af39b74856a53c8de0
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2023-12-05 00:43:20 +0100

    hello from class component

--- JS_learning/React/hello-world/src/App.jsx
+++ JS_learning/React/hello-world/src/App.jsx
@@ -1,40 +1,12 @@
-import React from "react"
+import React from "react";
+import Welcome from "./components/Welcome";
 
 export default function App() {
 
-    const aboutMe = {
-        firstName: 'Zakaria', 
-        lastName: 'Jaddad', 
-        age: 20
-    }
 
-    function greeting({ firstName, lastName }) {
-        /* 
-        returns gretting to user by getting
-            - firstName
-            - lastName
-            */
-
-        return (
-            <div>
-                <ul className="
-                    px-4 
-                    list-none
-                ">
-                    <li> { firstName } </li>
-                    <li> { lastName } </li>
-                </ul>
-            </div>
-        )
-
-    }
-    
     return (
-        <div>
-            <h1>Hello to you, My Name is : </h1>
-            { greeting(aboutMe) }
-        </div>
-    )
+        <>
+            <Welcome />
+        </>
+    );
 }
-
-
---
+++ JS_learning/React/hello-world/src/components/Welcome.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Welcome extends React.Component {
+
+    render() {
+        return (
+            <>
+                <h1>hellos from Class Component</h1>
+            </>
+       )
+    }
+}
+
+export default Welcome;
