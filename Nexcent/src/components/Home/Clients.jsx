1a7e34458a9fb8b706211c0307df2895ce27919e
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2023-12-08 18:22:52 +0100

    Our Clients infinet scroll is done

--- Nexcent/src/components/Home/Clients.jsx
+++ Nexcent/src/components/Home/Clients.jsx
@@ -32,6 +32,7 @@ const Clients = () => {
 
   return (
     <section className=" my-7 container mx-auto">
+
       <h2
         className="
         text-center
@@ -53,17 +54,48 @@ const Clients = () => {
         We have been working with some Fortune 500+ clients
       </p>
 
-      {/* Partners */}
+      {/* Partners section*/}
+      <section className="
+        partners-container
+        flex
+        overflow-hidden
+        relative
+        before:content-[''] before:h-full before:w-1/6 before:absolute before:top-0 before:left-0 before:bg-gradient-to-r from-white to-transparent z-10
+        after:content-[''] after:h-full after:w-1/6 after:absolute after:top-0 after:right-0 after:bg-gradient-to-l from-white to-transparent z-10
+      ">
+
+        {/* start partners */}
+
       <div
         className="
-        flex  
+        flex
         h-16
-        justify-between
-        px-4
+        justify-around
+        min-w-full
+        animate-carusel
+        -z-10
       "
       >
         <Partners logos={logos} />
       </div>
+
+
+      <div
+        className="
+        flex
+        h-16
+        justify-around
+        min-w-full
+        animate-carusel
+        -z-10
+      "
+      >
+        <Partners logos={logos} />
+      </div>
+
+        {/* start partners */}
+      </section>
+      
     </section>
   );
 };
--- Nexcent/tailwind.config.js
+++ Nexcent/tailwind.config.js
@@ -1,16 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       height: {
-        31: '31rem'
-      }
+        31: "31rem",
+      },
+      animation: {
+        carusel: "carusel 08s linear infinite",
+      },
+      keyframes: {
+        carusel: {
+          "0%": { translate: "0%" },
+          // "90%": { translate: "-100%" },
+          "100%": { translate: "-100%" },
+        },
+      },
     },
   },
   plugins: [],
-}
-
+};
