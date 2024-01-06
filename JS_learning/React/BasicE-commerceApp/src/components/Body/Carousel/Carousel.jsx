9837154eae0c610432d1409228be0dfc7eb7da6c
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2024-01-06 23:31:27 +0100

    Refactor: Add Arrow Buttons To Carousel

--- JS_learning/React/BasicE-commerceApp/src/components/Body/Carousel/Carousel.jsx
+++ JS_learning/React/BasicE-commerceApp/src/components/Body/Carousel/Carousel.jsx
@@ -1,54 +1,43 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import arrowRight from "/images/carousel/svg/arrow-right.svg"
-import arrowLeft from "/images/carousel/svg/arrow-left.svg"
+import arrowRight from "/images/carousel/svg/arrow-right.svg";
+import arrowLeft from "/images/carousel/svg/arrow-left.svg";
 import carouselData from "./CarouselData";
 
 const Carousel = () => {
-
   const settings = {
-    // dots: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
     infinite: true,
-    autoplay: true,
-    autoplaySpeed: 1000,
     nextArrow: (
       <div>
-        <div className="next-slick-arrow">
-            <svg xmlns="http://www.w3.org/2000/svg" stroke="black" height="24" viewBox="0 -960 960 960" width="24"><path d="m242-200 200-280-200-280h98l200 280-200 280h-98Zm238 0 200-280-200-280h98l200 280-200 280h-98Z"/></svg>
-        </div>
+        <img src={arrowRight} />
       </div>
     ),
 
     prevArrow: (
       <div>
-        <div className="next-slick-arrow rotate-180">
-          <svg xmlns="http://www.w3.org/2000/svg" stroke="black" height="24" viewBox="0 -960 960 960" width="24"><path d="m242-200 200-280-200-280h98l200 280-200 280h-98Zm238 0 200-280-200-280h98l200 280-200 280h-98Z"/></svg>
-        </div>
+        <img src={arrowLeft} />
       </div>
     ),
   };
 
   const Slides = carouselData.map((Slide, SlideIndex) => {
     return (
-      <div
-        key={SlideIndex}
-        className="slides-container w-full"
-      >
-      <img
+      <div key={SlideIndex} className="slides-container">
+        <img
           src={Slide.imageSrc}
           alt={Slide.imageAlt}
         />
       </div>
-    )
-  })
+    );
+  });
 
   return (
     <div>
-      <Slider {...settings}>
+      <Slider {...settings} className="">
         {Slides}
       </Slider>
     </div>
