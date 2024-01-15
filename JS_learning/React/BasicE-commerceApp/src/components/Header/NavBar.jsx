8b87863591a2cc4aecfc75f301c69f964c46b75c
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2024-01-15 19:34:54 +0100

    Update To NavBar Wish List Icon

--- JS_learning/React/BasicE-commerceApp/src/components/Header/NavBar.jsx
+++ JS_learning/React/BasicE-commerceApp/src/components/Header/NavBar.jsx
@@ -1,19 +1,22 @@
+import wishlist from "/images/header/shopping-cart.svg";
 const NavBar = () => {
-    return (
-        <div className="w-[600px] ">
-            <ul className="w-full flex justify-around items-center">
-                <li>
-                    <a href="">Press Me</a>
-                </li>
-                <li>
-                    <a href="">Press Me</a>
-                </li>
-                <li>
-                    <a href="">Press Me</a>
-                </li>
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div className="w-[600px] ">
+      <ul className="w-full flex justify-end items-center gap-[30px]">
+        <li>
+          <a href="">Press Me</a>
+        </li>
+        <li>
+          <a href="">Press Me</a>
+        </li>
+        <li>
+          <a href="">
+            <img className="w-[30px] h-[30px]" src={wishlist} alt="shooping card" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default NavBar;
