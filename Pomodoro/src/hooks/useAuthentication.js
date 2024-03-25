dc78c1ca824acfdc82dace088c4fa60d12521e43
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2024-03-25 23:25:57 +0000

    [Pomodoro]Todoist Api: Save Token in localStorage

--- Pomodoro/src/hooks/useAuthentication.js
+++ Pomodoro/src/hooks/useAuthentication.js
@@ -1,13 +1,24 @@
+import Todoist from "../api/todoist/Todoist";
 import info from "../api/todoist/info";
+import getDataFromLocalStorage from "../app/util/localStorage/getDataFromLocalStorage";
 import saveDataToLocalStorage from "../app/util/localStorage/saveDataTolocalStorage";
 import { useLocation } from "react-router-dom";
 
 const KEY = "Authentication";
+const saveUserToken = async (code) => {
+  const userToken = await Todoist.getToken(code);
+  saveDataToLocalStorage(KEY, userToken.access_token);
+};
+
 const useAuthentication = () => {
+  const isTokenExist = getDataFromLocalStorage(KEY, null);
+  if (isTokenExist !== null) {
+    console.log("Happend");
+    return false;
+  }
+
   const location = useLocation();
   const params = new URLSearchParams(location.search);
-
-  //
   const code = params.get("code");
   const state = params.get("state");
 
@@ -17,7 +28,7 @@ const useAuthentication = () => {
   if (state !== info.state) {
     return false;
   }
-  saveDataToLocalStorage(KEY, code);
+  saveUserToken(code);
   return true;
 };
 export default useAuthentication;
