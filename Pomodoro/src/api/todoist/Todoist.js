dd837b281ef6292b5ced30c5f96994fd79db1eae
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2024-03-25 23:25:13 +0000

    [Pomodoro]Todoist Api: Token Request

--- Pomodoro/src/api/todoist/Todoist.js
+++ Pomodoro/src/api/todoist/Todoist.js
@@ -1,4 +1,18 @@
+import info from "./info";
 const Todoist = {
+  getToken: async (code) => {
+    const formData = new FormData();
+    formData.append("client_id", info.clientID);
+    formData.append("client_secret", info.secret);
+    formData.append("code", code);
 
-}
+    const response = await fetch("https://todoist.com/oauth/access_token", {
+      method: "POST",
+      body: formData,
+    });
+    if (!response.ok) throw "Unable to procces request";
+    const data = await response.json();
+    return data;
+  },
+};
 export default Todoist;
