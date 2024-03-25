cabad9e8843d645614e7ad0ae0172728c3b43525
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2024-03-25 18:33:48 +0000

    [Pomodoro]: initial todoist api

---
+++ Pomodoro/src/api/todoist/Todoist.js
@@ -0,0 +1,4 @@
+const Todoist = {
+
+}
+export default Todoist;
---
+++ Pomodoro/src/api/todoist/info.js
@@ -0,0 +1,6 @@
+const info = {
+  clientID: "9d9c9a3377da4150a8daf1d8ae80bfdd",
+  permissionScopes: "data:read_write,data:delete",
+  state: "loopmakepomodorodady",
+};
+export default info;
