437039c0d20399ff961b955b1a81b1c13acef1d9
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2024-05-13 18:01:03 +0100

    [MyPomodoro]reload after authentication

--- Pomodoro/src/hooks/useAuthentication.js
+++ Pomodoro/src/hooks/useAuthentication.js
@@ -1,13 +1,25 @@
-import Todoist from "../api/todoist/Todoist";
-import info from "../api/todoist/info";
 import getDataFromLocalStorage from "../app/util/localStorage/getDataFromLocalStorage";
 import saveDataToLocalStorage from "../app/util/localStorage/saveDataTolocalStorage";
-import { useLocation } from "react-router-dom";
+import info from "../api/todoist/info";
+import Todoist from "../api/todoist/Todoist";
+import {
+  redirect,
+  unstable_HistoryRouter,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
+import { toast } from "sonner";
 
 const KEY = "todoist_token";
 const saveUserToken = async (code) => {
-  const userToken = await Todoist.getToken(code);
-  saveDataToLocalStorage(KEY, userToken.access_token);
+  const { isSuccess, message, access_token } = await Todoist.getToken(code);
+  if (isSuccess) {
+    saveDataToLocalStorage(KEY, access_token);
+    toast.success(message);
+    window.location.href = window.location.pathname;
+  }
+  toast.error(message);
+  return false;
 };
 
 const useAuthentication = () => {
@@ -17,11 +29,10 @@ const useAuthentication = () => {
   }
 
   const location = useLocation();
+
   const params = new URLSearchParams(location.search);
   const code = params.get("code");
   const state = params.get("state");
-
-  info.code = code;
 
   if (code === null || state === null) {
     return false;
@@ -30,7 +41,6 @@ const useAuthentication = () => {
     return false;
   }
   saveUserToken(code);
-  return true;
 };
 
 export default useAuthentication;
